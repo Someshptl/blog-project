c43ae3ccd9cfa1c366c5cb9bb12d13378fec05a8
Somesh <somshptl@gmail.com>
2024-07-27 19:49:25 +0530

    Implemented new features for the defect.

---
+++ Blog-App/src/main/java/com/myblog/TestClass.java
@@ -0,0 +1,11 @@
+package com.myblog;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        TestClass testClass = new TestClass();
+        testClass.getClass();
+        System.out.println(testClass);
+    }
+}
